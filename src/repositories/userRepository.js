e9fa5fffcdf6facb7469ba2c7cc0e7971568087a
yeonji123 <76023703+yeonji123@users.noreply.github.com>
2024-11-24 18:21:21 +0900

    models -> repository로 대체

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -2,7 +2,7 @@ const {db} = require('./config/db');
 
 
 const getAllUsers = async() => {
-    const [rows] = await db.query('SELECT * FROM users');
+    const [rows] = await db.query('SELECT * FROM LC_USER_T');
     return rows;
 }
 
@@ -10,7 +10,6 @@ const getUserById = async(id) => {
     const [rows] = await db.query('SELECT * FROM users WHERE id = ?', [id]);
     return rows[0];
 }
-
 
 module.exports = {
     getAllUsers, getUserById
