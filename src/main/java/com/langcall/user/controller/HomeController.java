592b4f006859aa9f64218ec7b59090fd558adac6
yeonji123 <76023703+yeonji123@users.noreply.github.com>
2025-04-07 20:34:01 +0900

    feat: Application.run 설정 및 hello

--- src/main/java/com/langcall/user/controller/HomeController.java
+++ src/main/java/com/langcall/user/controller/HomeController.java
@@ -1,13 +1,22 @@
 package com.langcall.user.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
-//    @RequestMapping(value="/login")x
+    @GetMapping("/")
+    public String maincontroller(){
+        return "/";
+    }
 
+    @GetMapping("hello")
+    public String hello(){
+        return "hello world";
+    }
 
 
 }
